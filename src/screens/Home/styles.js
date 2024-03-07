8afdf90166554be8a48d1951dec03172d735f856
Joao Oliveira <joaosantos@aluno.senai.br>
2024-03-07 08:08:28 -0300

    create css

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,10 +1,5 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { StyleSheet } from "react-native";
 
-export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
-}
+const styles = StyleSheet.create({});
+
+export default styles;
