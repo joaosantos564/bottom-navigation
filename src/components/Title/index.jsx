6efb6fa9e2276f2cea057c4f21e52f987983240a
Joao Oliveira <joaosantos@aluno.senai.br>
2024-03-07 08:13:29 -0300

    component title

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+const Title = () => {
+  return (
+    <View>
+      <Text style={styles.title1}>styles</Text>
+    </View>
+  );
+};
+
+export default Title;
---
+++ src/components/Title/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  title1: {
+    fontSize: 30,
+  },
+});
+
+export default styles;
