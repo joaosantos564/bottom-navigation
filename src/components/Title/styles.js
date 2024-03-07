06c5166e9c46ab7274e78b11902f05b005b70785
Joao Oliveira <joaosantos@aluno.senai.br>
2024-03-07 08:33:29 -0300

    feat: add react nav

--- src/components/Title/styles.js
+++ src/components/Title/styles.js
@@ -1,6 +1,10 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    width: 200,
+  },
   title1: {
     fontSize: 30,
     color: "#c3c3",
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import styles from "./styles";
+import Title from "../../components/Title";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Home />
+      <Title titulo={"Home"} />
     </View>
   );
 }
