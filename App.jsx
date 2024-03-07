eab32601761d81bba257505cba7f16959317a9c9
Joao Oliveira <joaosantos@aluno.senai.br>
2024-03-07 08:58:57 -0300

    create screens

--- App.jsx
+++ App.jsx
@@ -3,6 +3,8 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./src/screens/Home";
+import Contact from "./src/screens/Contact";
+import Profile from "./src/screens/Profile";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,6 +12,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Contact" component={Contact} />
         <Tab.Screen name="Home" component={Home} />
       </Tab.Navigator>
       <StatusBar style="auto" />
